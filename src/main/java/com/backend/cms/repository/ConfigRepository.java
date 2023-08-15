056a36ac5397c1ff0905930c673e43bab7d23b82
LoredanaCicati <loredanacicati@gmail.com>
2023-08-15 12:45:23 +0200

    update config repository class

--- src/main/java/com/backend/cms/repository/ConfigRepository.java
+++ src/main/java/com/backend/cms/repository/ConfigRepository.java
@@ -3,8 +3,10 @@ package com.backend.cms.repository;
 import com.backend.cms.model.Config;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConfigRepository extends MongoRepository<Config, String>, QuerydslPredicateExecutor<Config> {
 
-    Config findFirst();
+    Config findFirstBy();
 }
