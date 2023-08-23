29016349b562d17bca657d0769451d8b20836dee
LoredanaCicati <loredanacicati@gmail.com>
2023-08-23 18:36:13 +0200

    add more tests for login + improve old tests

--- src/test/java/com/backend/cms/controller/AdminInitControllerTest.java
+++ src/test/java/com/backend/cms/controller/AdminInitControllerTest.java
@@ -1,39 +1,59 @@
 package com.backend.cms.controller;
 
 import com.backend.cms.dto.RegisterUserDTO;
+import com.backend.cms.repository.ConfigRepository;
 import com.backend.cms.request.CreateInitAdminRequest;
+import com.backend.cms.service.AdminInitializationService;
+import com.backend.cms.service.AuthService;
+import com.backend.cms.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.Objects;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class AdminInitControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @InjectMocks
+    private AdminInitController adminInitController;
+
+    @MockBean
+    private UserService userService;
+    @MockBean
+    AdminInitializationService adminInitializationService;
+
+    @MockBean
+    private ConfigRepository configRepository;
+
+    @MockBean
+    private AuthService authService;
 
     @Test
     void testCreateAdmin() throws Exception {
+        // Mocking the behavior of userService.isInitialized()
+        when(adminInitializationService.isAdminInitialized()).thenReturn(false);
+
         CreateInitAdminRequest request = new CreateInitAdminRequest();
         // Set properties for the request object
         request.setFirstName("admin");
@@ -44,39 +64,72 @@ class AdminInitControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
         String requestJson = objectMapper.writeValueAsString(request);
 
+        // Mocking the behavior of userService.createInitialAdmin()
+        RegisterUserDTO mockResult = new RegisterUserDTO();
+        mockResult.setUserId("mockUserId");
+        mockResult.setFirstName(request.getFirstName());
+        mockResult.setLastName(request.getLastName());
+        mockResult.setEmail(request.getEmail());
+        mockResult.setPassword(passwordEncoder.encode(request.getPassword()));
+        when(adminInitializationService.initializeAdmin(any(CreateInitAdminRequest.class))).thenReturn(mockResult);
+
         // Perform the POST request and get the result
         MvcResult response = mvc.perform(
                         MockMvcRequestBuilders.post("/api/v1/admin/initialize")
                                 .contentType("application/json")
                                 .content(requestJson))
                 .andReturn();
+        // Retrieve the actual status code from the response
+        int actualStatusCode = response.getResponse().getStatus();
+        // Assert the status code
+        assertEquals(HttpStatus.CREATED.value(), actualStatusCode);
+
+        // Assert other parts of the response
+        String json = response.getResponse().getContentAsString();
+        RegisterUserDTO result = objectMapper.readValue(json, RegisterUserDTO.class);
+
+        assertNotNull(result.getUserId());
+        assertEquals("admin", result.getFirstName());
+        assertEquals("Loredana", result.getLastName());
+        assertEquals("admin@gmail.com", result.getEmail());
+
+        // Assert the hashed password
+        assertTrue(passwordEncoder.matches("adminPassword1@q", result.getPassword()));
+    }
+
+    @Test
+    void testCreateAdmin_AlreadyInitialized() throws Exception {
+        // Mocking the behavior of adminInitializationService.isAdminInitialized()
+        when(adminInitializationService.isAdminInitialized()).thenReturn(true);
+
+        CreateInitAdminRequest request = new CreateInitAdminRequest();
+        // Set properties for the request object
+        request.setFirstName("admin");
+        request.setLastName("Loredana");
+        request.setEmail("admin@gmail.com");
+        request.setPassword("adminPassword1@q");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // Mocking the behavior of adminInitializationService.initializeAdmin() to throw BadRequestException
+        when(adminInitializationService.initializeAdmin(any(CreateInitAdminRequest.class)))
+                .thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Admin already initialized"));
+
+        // Perform the POST request and expect a BAD_REQUEST status
+        MvcResult response = mvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/admin/initialize")
+                                .contentType("application/json")
+                                .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
 
         // Retrieve the actual status code from the response
         int actualStatusCode = response.getResponse().getStatus();
-        if (actualStatusCode == HttpStatus.CREATED.value()) {
 
-            // Assert the status code
-            assertEquals(HttpStatus.CREATED.value(), response.getResponse().getStatus());
-
-            // Assert other parts of the response
-            String json = response.getResponse().getContentAsString();
-            RegisterUserDTO result = objectMapper.readValue(json, RegisterUserDTO.class);
-
-            assertNotNull(result.getUserId());
-            assertEquals("admin", result.getFirstName());
-            assertEquals("Loredana", result.getLastName());
-            assertEquals("admin@gmail.com", result.getEmail());
-
-            // Assert the hashed password
-            assertTrue(passwordEncoder.matches("adminPassword1@q", result.getPassword()));
-        } else {
-            // Assert the status code (should be 400 as admin is already initialized)
-            assertEquals(HttpStatus.BAD_REQUEST.value(), response.getResponse().getStatus());
-
-            // Verify the error message
-            String errorMessage = Objects.requireNonNull(response.getResolvedException()).getMessage();
-            assertTrue(errorMessage.contains("Admin already initialized"));
-        }
+        // Assert the status code is BAD_REQUEST
+        assertEquals(HttpStatus.BAD_REQUEST.value(), actualStatusCode);
     }
 
     @Test
--- src/test/java/com/backend/cms/controller/UserControllerTest.java
+++ src/test/java/com/backend/cms/controller/UserControllerTest.java
@@ -9,14 +9,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,7 +25,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class UserControllerTest {
 
@@ -36,7 +34,7 @@ class UserControllerTest {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
-    @Mock
+    @MockBean
     private UserRepository userRepository; // Mocked UserRepository
 
     @InjectMocks
@@ -45,70 +43,64 @@ class UserControllerTest {
 
     @Test
     void testFindUserById() throws Exception {
-        String userId = "ubcy8l";
+        String userId = "ubcy8c";
 
         // Generate a valid token
-        String token = jwtTokenUtil.generateToken(userId); // Replace with a valid token
+        String token = jwtTokenUtil.generateToken(userId);
 
-        // Define the behavior of userRepository.findByUserId()
-        User mockUser = new User(); // Replace with a mock User instance
+        // Defining the behavior of userRepository.findByUserId() to return a mockUser
+        User mockUser = new User();
         when(userRepository.findByUserId(eq(userId))).thenReturn(mockUser);
 
+        // Perform the GET request
+        MvcResult response = mvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/users/{id}", userId)
+                                .header("Authorization", "Bearer " + token))
+                .andReturn();
+        System.out.println("body: " + response.getResponse().getContentAsString());
+
+        // Retrieve the actual status code from the response
+        int actualStatusCode = response.getResponse().getStatus();
+        System.out.println("status: " + actualStatusCode);
+
+        // User with specified id exists
+        if (userRepository.findByUserId(userId) != null) {
+            // Assert the status code is OK when the user exists
+            assertEquals(HttpStatus.OK.value(), actualStatusCode);
+        } else {
+            // Assert the status code is NOT_FOUND when the user doesn't exist
+            assertEquals(HttpStatus.NOT_FOUND.value(), actualStatusCode);
+        }
+    }
+
+
+    @Test
+    void testFindUserById_NotFound() throws Exception {
+        String userId = "ubcy8c";
+
+        // Generate a valid token
+        String token = jwtTokenUtil.generateToken(userId);
+
+        // Defining the behavior of userRepository.findByUserId()
+        when(userRepository.findByUserId(eq(userId))).thenReturn(null);
         try {
             // Perform the GET request
             MvcResult response = mvc.perform(
                             MockMvcRequestBuilders.get("/api/v1/users/{id}", userId)
                                     .header("Authorization", "Bearer " + token))
                     .andReturn();
-
             // Retrieve the actual status code from the response
             int actualStatusCode = response.getResponse().getStatus();
-            // User with specified id exists
-            if (userRepository.findByUserId(userId) != null) {
-                // Assert the status code is OK when the user exists
-                assertEquals(HttpStatus.OK.value(), actualStatusCode);
-            } else {
-                // Assert the status code is UNAUTHORIZED when the user doesn't exist
-                assertEquals(HttpStatus.UNAUTHORIZED.value(), actualStatusCode);
-            }
+
+            // Assert the status code is NOT_FOUND when the user doesn't exist
+            assertEquals(HttpStatus.NOT_FOUND.value(), actualStatusCode);
+
         } catch (org.springframework.security.core.userdetails.UsernameNotFoundException ex) {
             // Assert that the exception was caught and response status is UNAUTHORIZED
             assertEquals(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.value());
         }
     }
 
-    //    @Test
-//    void testSuccessfulLogin() throws Exception {
-//        // Create a LoginRequest object with valid credentials
-//        LoginRequest loginRequest = new LoginRequest();
-//        loginRequest.setEmail("new@example.com");
-//        loginRequest.setPassword("newpassW@1");
-//
-//        // Convert the LoginRequest object to a JSON string
-//        String requestBody = new ObjectMapper().writeValueAsString(loginRequest);
-//
-//        // Create a mocked User instance with matching email and password
-//        User mockUser = new User();
-//        mockUser.setUserId("someUserId");
-//        mockUser.setEmail("new@example.com"); // Set the email to match the request
-//        mockUser.setPassword(new BCryptPasswordEncoder().encode("newpassW@1")); // Set the password to match the request
-//
-//        // Define the behavior of userRepository.findByEmail()
-//        when(userRepository.findByEmail(eq(loginRequest.getEmail()))).thenReturn(mockUser);
-//
-//        // Perform the POST request
-//        MvcResult response = mvc.perform(
-//                        MockMvcRequestBuilders.post("/api/v1/users/login")
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                                .content(requestBody))
-//                .andReturn();
-//
-//        // Retrieve the actual status code from the response
-//        int actualStatusCode = response.getResponse().getStatus();
-//
-//        // Assert the status code is OK for successful login
-//        assertEquals(HttpStatus.OK.value(), actualStatusCode);
-//    }
     @Test
     void testSuccessfulLogin() throws Exception {
         // Create a LoginRequest object with valid credentials
@@ -152,12 +144,12 @@ class UserControllerTest {
             // Assert the status code is OK for successful login
             assertEquals(HttpStatus.OK.value(), actualStatusCode);
         } else {
-            // Request body doesn't match mock user's credentials, should result in UNAUTHORIZED
-            int expectedStatusCode = HttpStatus.UNAUTHORIZED.value();
+            // Request body doesn't match mock user's credentials, should result in NOT_FOUND
+            int expectedStatusCode = HttpStatus.NOT_FOUND.value();
             System.out.println("expected status: " + expectedStatusCode);
 
-            // Assert the status code is UNAUTHORIZED for incorrect credentials
-            assertEquals(expectedStatusCode, HttpStatus.UNAUTHORIZED.value());
+            // Assert the status code is NOT_FOUND for incorrect credentials
+            assertEquals(expectedStatusCode, HttpStatus.NOT_FOUND.value());
         }
     }
 
@@ -185,22 +177,23 @@ class UserControllerTest {
         // Retrieve the actual status code from the response
         int actualStatusCode = response.getResponse().getStatus();
         System.out.println("status: " + actualStatusCode);
-        // Assert the status code is UNAUTHORIZED for unsuccessful login
-        assertEquals(HttpStatus.UNAUTHORIZED.value(), actualStatusCode);
+        // Assert the status code is NOT_FOUND for unsuccessful login
+        assertEquals(HttpStatus.NOT_FOUND.value(), actualStatusCode);
     }
 
     @Test
     void testIncorrectEmailLogin() throws Exception {
         // Create a LoginRequest object with incorrect email
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setEmail("nonexistent@example.com"); // An email that doesn't exist in the mocked repository
-        loginRequest.setPassword("newpassW@1");
+        // Setting an email that doesn't exist in the mocked repository
+        loginRequest.setEmail("nonexistent123@example.com");
+        loginRequest.setPassword("newpassW@12");
 
         // Convert the LoginRequest object to a JSON string
         String requestBody = new ObjectMapper().writeValueAsString(loginRequest);
         System.out.println("request body: " + requestBody);
 
-        // Define the behavior of userRepository.findByEmail()
+        // Defining the behavior of userRepository.findByEmail()
         when(userRepository.findByEmail(eq(loginRequest.getEmail()))).thenReturn(null);
 
         // Perform the POST request
@@ -214,8 +207,8 @@ class UserControllerTest {
         // Retrieve the actual status code from the response
         int actualStatusCode = response.getResponse().getStatus();
         System.out.println("status: " + actualStatusCode);
-        // Assert the status code is UNAUTHORIZED for incorrect email
-        assertEquals(HttpStatus.UNAUTHORIZED.value(), actualStatusCode);
+        // Assert the status code is NOT_FOUND for incorrect email
+        assertEquals(HttpStatus.NOT_FOUND.value(), actualStatusCode);
     }
 
     @Test
@@ -231,9 +224,9 @@ class UserControllerTest {
         // Create a mocked User instance
         User mockUser = new User();
         mockUser.setUserId("someUserId");
-        mockUser.setPassword(new BCryptPasswordEncoder().encode("correctPassword")); // Set the actual correct password
+        mockUser.setPassword(new BCryptPasswordEncoder().encode("validP@12")); // Set the actual correct password
 
-        // Define the behavior of userRepository.findByEmail()
+        // Defining the behavior of userRepository.findByEmail()
         when(userRepository.findByEmail(eq(loginRequest.getEmail()))).thenReturn(mockUser);
 
         // Perform the POST request
@@ -266,8 +259,40 @@ class UserControllerTest {
         // Retrieve the actual status code from the response
         int actualStatusCode = response.getResponse().getStatus();
         System.out.println("status: " + actualStatusCode);
-        // Assert the status code is UNAUTHORIZED for empty body
-        assertEquals(HttpStatus.UNAUTHORIZED.value(), actualStatusCode);
+        // Assert the status code is BAD_REQUEST for empty body
+        assertEquals(HttpStatus.BAD_REQUEST.value(), actualStatusCode);
+    }
+
+    @Test
+    void testInvalidEmailFormat() throws Exception {
+        // Create a LoginRequest object with an invalid email format
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("invalid-email"); // Invalid email format
+        loginRequest.setPassword("passworD@123");
+
+        // Convert the LoginRequest object to a JSON string
+        String requestBody = new ObjectMapper().writeValueAsString(loginRequest);
+        System.out.println("request body: " + requestBody);
+        // Create a mocked User instance
+        User mockUser = new User();
+        mockUser.setEmail("admin@example.com");
+        mockUser.setPassword(new BCryptPasswordEncoder().encode("passworD@123"));
+
+        // Define the behavior of userRepository.findByEmail()
+        when(userRepository.findByEmail(eq(loginRequest.getEmail()))).thenReturn(mockUser);
+
+        // Perform the POST request
+        MvcResult response = mvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/users/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andReturn();
+        System.out.println("body: " + response.getResponse().getContentAsString());
+        // Retrieve the actual status code from the response
+        int actualStatusCode = response.getResponse().getStatus();
+        System.out.println("status: " + actualStatusCode);
+        // Assert the status code is BAD_REQUEST for invalid email format
+        assertEquals(HttpStatus.BAD_REQUEST.value(), actualStatusCode);
     }
 
 }
