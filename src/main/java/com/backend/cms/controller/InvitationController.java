04917bb69b5969ccf9913cd0ad9b73236e3d2a5d
LoredanaCicati <74156276+LoredanaCicati@users.noreply.github.com>
2023-09-27 18:27:29 +0200

    add controller for sending email invitations

---
+++ src/main/java/com/backend/cms/controller/InvitationController.java
@@ -0,0 +1,30 @@
+package com.backend.cms.controller;
+
+import com.backend.cms.request.CreateUserRequest;
+import com.backend.cms.service.InvitationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/invitations")
+public class InvitationController {
+
+    @Autowired
+    private InvitationService invitationService;
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendInvitationEmail(
+            @Valid @RequestBody CreateUserRequest request) {
+        try {
+            invitationService.sendInvitation(request);
+            return ResponseEntity.ok("Invitation sent successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+}
