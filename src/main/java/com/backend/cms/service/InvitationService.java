7f3e3a85022d127b492f4e8854cf82178a5d8d60
LoredanaCicati <74156276+LoredanaCicati@users.noreply.github.com>
2023-10-02 18:17:23 +0200

    verify if token is expired, resend invitation
    -Verified if user token has expired
    - Resend invitation for expired token

--- src/main/java/com/backend/cms/service/InvitationService.java
+++ src/main/java/com/backend/cms/service/InvitationService.java
@@ -1,11 +1,13 @@
 package com.backend.cms.service;
 
+import com.backend.cms.exceptions.NotFoundException;
+import com.backend.cms.model.User;
+import com.backend.cms.repository.UserRepository;
 import com.backend.cms.request.CreateUserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
@@ -17,17 +19,17 @@ public class InvitationService {
     @Autowired
     private UserService userService;
 
-    public void sendInvitation(CreateUserRequest request) {
+    @Autowired
+    private UserRepository userRepository;
 
+    public void sendInvitation(CreateUserRequest request) {
         // Generate a unique token
         String token = generateUniqueToken();
 
-        // Store invitation information
-        userService.createUser(request);
+        userService.createUser(request, token);
 
         // Send invitation email
         sendEmail(request.getEmail(), token);
-
     }
 
     private void sendEmail(String recipientEmail, String token) {
@@ -45,6 +47,35 @@ public class InvitationService {
         long expirationTimeMillis = System.currentTimeMillis() + (24 * 60 * 60 * 1000);
         String token = UUID.randomUUID().toString().replaceAll("-", "");
         return token + "_" + expirationTimeMillis;
+    }
+
+    private boolean isTokenExpired(String token) {
+        String[] parts = token.split("_");
+        if (parts.length != 2) {
+            return true;
+        }
+
+        long expirationTimeMillis = Long.parseLong(parts[1]);
+        return System.currentTimeMillis() > expirationTimeMillis;
+    }
+
+    public boolean resendInvitation(String userId) {
+        User user = userRepository.findByUserId(userId);
+        if (user != null) {
+            if (isTokenExpired(user.getToken())) {
+                String newToken = generateUniqueToken();
+                user.setToken(newToken);
+                userRepository.save(user);
+                sendEmail(user.getEmail(), newToken);
+                // Invitation resent successfully
+                return true;
+            } else {
+                // Invitation is still valid
+                return false;
+            }
+        } else {
+            throw new NotFoundException();
+        }
     }
 
 }
