92f769f1e96e5b53bfbbfdcd145205b597dc9606
LoredanaCicati <74156276+LoredanaCicati@users.noreply.github.com>
2023-12-14 18:04:47 +0100

    update regex pattern

--- src/main/java/com/backend/cms/service/PostService.java
+++ src/main/java/com/backend/cms/service/PostService.java
@@ -168,7 +168,8 @@ public class PostService {
         if (maximumLength != null && textValue.length() > maximumLength) {
             throw new IllegalArgumentException("Attribute '" + collectionAttribute.getName() + "' must have a maximum length of " + maximumLength);
         }
-        String regexPattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9\\s.,!?:-]*$";
+        String regexPattern = "^(?=.*[a-zA-Z])[a-zA-Z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$";
+
 
         if (textValue.length() > 0 && !textValue.matches(regexPattern)) {
             throw new IllegalArgumentException("Attribute '" + collectionAttribute.getName() + "' must match a certain pattern ");
